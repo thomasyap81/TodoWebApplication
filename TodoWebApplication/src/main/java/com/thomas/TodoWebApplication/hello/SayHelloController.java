6403b42c87473f47b1c32c0ad4eb612740fbc7e6
Thomas Yap <yaptathong@gmail.com>
2024-04-20 20:56:10 +0800

    Use @ResponseBody, @Controller and @RequestMapping

---
+++ TodoWebApplication/src/main/java/com/thomas/TodoWebApplication/hello/SayHelloController.java
@@ -0,0 +1,15 @@
+package com.thomas.TodoWebApplication.hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class SayHelloController {
+	
+	@RequestMapping("say-hello")
+	@ResponseBody
+	public String sayHello() {
+		return "Hello! what are you  learning today?";
+	}
+}
