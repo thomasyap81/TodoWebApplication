262e50066f2471f573ecb59566967ff4404d073c
Thomas Yap <yaptathong@gmail.com>
2024-04-21 13:37:29 +0800

    import logging framework slf4j

--- TodoWebApplication/src/main/java/com/thomas/TodoWebApplication/login/LoginController.java
+++ TodoWebApplication/src/main/java/com/thomas/TodoWebApplication/login/LoginController.java
@@ -1,5 +1,7 @@
 package com.thomas.TodoWebApplication.login;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,10 +10,17 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class LoginController {
 	
+	private Logger logger = LoggerFactory.getLogger(getClass());
+	
 	//http://localhost:8080/login?nameid=thomas
 	@RequestMapping("login")
 	public String loginWeb(@RequestParam String nameid, ModelMap model) {
 		model.put("nameJSP", nameid);
+		
+		logger.debug("Request param is {}", nameid);
+		logger.info("info");
+		logger.warn("warn");
+		
 		System.out.println("Welcome " + nameid);
 		return "login";
 	}
