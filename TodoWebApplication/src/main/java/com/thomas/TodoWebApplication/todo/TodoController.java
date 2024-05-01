03f15b5ea9ce7f3521b812586c03261f4d7f1663
Thomas Yap <yaptathong@gmail.com>
2024-05-01 23:57:58 +0800

    Add validation using Springboot starter validation

--- TodoWebApplication/src/main/java/com/thomas/TodoWebApplication/todo/TodoController.java
+++ TodoWebApplication/src/main/java/com/thomas/TodoWebApplication/todo/TodoController.java
@@ -30,14 +30,17 @@ public class TodoController {
 	
 	//get,post methods
 	@RequestMapping(value="/add-todo", method=RequestMethod.GET)
-	public String showNewTodoPage() {
+	public String showNewTodoPage(ModelMap model) {
+		String username =(String)model.get("name");
+		Todo todo = new Todo(0,username, "Default Description", LocalDate.now().plusYears(1),false);
+		model.put("todo",todo); // the "todo" must same name as modelAttribute="todo"
 		return "todo";
 	}
 	
 	@RequestMapping(value="/add-todo", method=RequestMethod.POST)
-	public String addNewTodo(@RequestParam String description, ModelMap model) {
+	public String addNewTodo(ModelMap model, Todo todo) {
 		String username =(String)model.get("name");
-		todoService.addTodo(username, description, LocalDate.now().plusYears(1), false);
+		todoService.addTodo(username, todo.getDescription(), LocalDate.now().plusYears(1), false);
 		return "redirect:list-todos";
 	}
 }
