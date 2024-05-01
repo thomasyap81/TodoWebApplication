f74f16320999725d3cc4f9fa66d4a61e080b177b
Thomas Yap <yaptathong@gmail.com>
2024-05-01 23:24:58 +0800

    enhance todoService to add Todo

--- TodoWebApplication/src/main/java/com/thomas/TodoWebApplication/todo/TodoController.java
+++ TodoWebApplication/src/main/java/com/thomas/TodoWebApplication/todo/TodoController.java
@@ -1,23 +1,25 @@
 package com.thomas.TodoWebApplication.todo;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 @Controller
 @SessionAttributes("my_name")
 public class TodoController {
 	
+	private TodoService todoService;
+
 	public TodoController(TodoService todoService) {
 		super();
 		this.todoService =todoService;
 	}
-	
-	private TodoService todoService;
 	
 	@RequestMapping("/list-todos")
 	public String listAllTodos(ModelMap model) {
@@ -33,7 +35,9 @@ public class TodoController {
 	}
 	
 	@RequestMapping(value="/add-todo", method=RequestMethod.POST)
-	public String addNewTodo() {
+	public String addNewTodo(@RequestParam String description, ModelMap model) {
+		String username =(String)model.get("name");
+		todoService.addTodo(username, description, LocalDate.now().plusYears(1), false);
 		return "redirect:list-todos";
 	}
 }
