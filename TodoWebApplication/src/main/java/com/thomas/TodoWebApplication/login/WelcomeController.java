9d66b2cf68749678285a135b2edead62fbdee241
Thomas Yap <yaptathong@gmail.com>
2024-05-09 19:23:52 +0800

    Prepare for Spring security

---
+++ TodoWebApplication/src/main/java/com/thomas/TodoWebApplication/login/WelcomeController.java
@@ -0,0 +1,19 @@
+package com.thomas.TodoWebApplication.login;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+@Controller
+@SessionAttributes("my_name")
+public class WelcomeController {
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String gotoWelcomePage(ModelMap model) {
+		model.put("my_name", "thomas");
+		return "welcome";
+	}
+}
